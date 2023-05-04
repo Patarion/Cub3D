9a3ced38c8607207a6893a59b5a676c5bd7b7336
Valerie <zoou2foo@gmail.com>
2023-05-04 13:38:37 -0400

    ajout de fichiers pour prep raycast

--- src/cube/init_game.c
+++ src/cube/init_game.c
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   init_game.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/01 11:37:06 by vjean             #+#    #+#             */
-/*   Updated: 2023/05/04 13:35:45 by vjean            ###   ########.fr       */
+/*   Created: 2023/05/04 13:22:58 by vjean             #+#    #+#             */
+/*   Updated: 2023/05/04 13:27:55 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/Cub3D.h"
+#include "../../inc/Cub3D.h"
 
-int main(int argc, char **argv) {
-	if (argc != 2)
-	{
-		printf("Veuillez fournir un fichier au format .cub");
-		return (-1);
-	}
-	if (ParseInfo(argv[1]) != 0)
-		printf("La map n'est pas valide ou n'a pas toutes les bonnes info");
-	else
-		start_mlx(); //start the "game"; open winden and have the map
-	return 0;
+void	start_mlx(void)
+{
+	
 }
--- src/raycasting/raycast.c
+++ src/raycasting/raycast.c
@@ -1,26 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   raycast.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/01 11:37:06 by vjean             #+#    #+#             */
-/*   Updated: 2023/05/04 13:35:45 by vjean            ###   ########.fr       */
+/*   Created: 2023/05/04 13:15:04 by vjean             #+#    #+#             */
+/*   Updated: 2023/05/04 13:17:13 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/Cub3D.h"
+#include "../../inc/Cub3D.h"
 
-int main(int argc, char **argv) {
-	if (argc != 2)
-	{
-		printf("Veuillez fournir un fichier au format .cub");
-		return (-1);
-	}
-	if (ParseInfo(argv[1]) != 0)
-		printf("La map n'est pas valide ou n'a pas toutes les bonnes info");
-	else
-		start_mlx(); //start the "game"; open winden and have the map
-	return 0;
-}
